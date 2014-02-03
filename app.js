23af1ed47ca3d4b078a819d6b393da0c357e78b1
andypandy <andrew.samuelsen@gmail.com>
2014-02-03 14:58:10 -0800

    Initial commit... searches for 1 page of 172s on aso and returns num of planes and average price

---
+++ app.js
@@ -0,0 +1,28 @@
+var scraper = require('scraper');
+
+scraper({
+		'uri': 'http://aso.com/listings/AircraftListings.aspx?mg_id=22&act_id=1&mmg=true',
+		'headers': {
+			'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
+		}
+	}, function(err, $) {
+		if (err) {console.log(err)};
+
+		var prices = [];
+		$('.photoListingsPrice').each(function() {
+			var dirtyPrice = $(this).text();
+			if(dirtyPrice != '$: Inquire') {
+				var trimmedPrice = $(this).text().replace(/\D/g, '');
+				prices.push(parseInt(trimmedPrice));
+			}
+		});
+
+		var sum = prices.reduce(function(a, b) { return a + b });
+		var avg = sum / prices.length;
+
+		console.log('Cessna 172 Prices');
+		console.log('Number of listings (with a price): ' + prices.length);
+		console.log('Average price: ' + avg);
+		
+	}
+);
